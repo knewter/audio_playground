f3660a46fca3374b7c670ec537c4b82bec2f258e
Josh Adams <josh@isotope11.com>
2014-06-06 21:56:03 -0500

    Use `spawn_executable` per true_droid

--- examples/play_file.exs
+++ examples/play_file.exs
@@ -1,4 +1,5 @@
 binary = File.read!("./examples/files/in.au")
-port = Port.open({:spawn, "pacat"}, [])
+pacat = :os.find_executable('pacat')
+port = Port.open({:spawn_executable, pacat}, [])
 send(port, {self, {:command, binary}})
 send(port, {self, {:command, binary}})
