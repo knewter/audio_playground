ac8fe8db61f79eb5093f631bb6a0ddfcf6a14734
Josh Adams <josh@isotope11.com>
2014-06-06 23:46:52 -0500

    Extract one more function for AsciiBarMeter

--- lib/audio_playground/visualization/ascii_bar_meter.ex
+++ lib/audio_playground/visualization/ascii_bar_meter.ex
@@ -2,18 +2,21 @@ defmodule AudioPlayground.Visualization.AsciiBarMeter do
   # Expects a half-width and a value between -1 and 1
   # FIXME: Typespecs
   def output(half_width, value) do
-    inner = for x <- 1..width(half_width), into: "" do
-              cond do
-                x == string_position(half_width, value) -> "x"
-                true -> " "
-              end
-            end
-    "[#{inner}]"
+    "[#{construct_meter(half_width, value)}]"
   end
 
-  def width(half_width), do: (half_width * 2) + 1
+  defp construct_meter(half_width, value) do
+    for x <- 1..width(half_width), into: "" do
+      cond do
+        x == string_position(half_width, value) -> "x"
+        true -> " "
+      end
+    end
+  end
 
-  def string_position(half_width, value) do
+  defp width(half_width), do: (half_width * 2) + 1
+
+  defp string_position(half_width, value) do
     position = :erlang.round(value * half_width)
     half_width + position + 1
   end
